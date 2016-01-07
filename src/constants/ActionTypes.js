445a0e66fb00b292ffd59839c57d2d3f006d4758
sylvaindethier <dethiersylvain@gmail.com>
2016-01-07 09:30:10 +0100

    add constants

---
+++ src/constants/ActionTypes.js
@@ -0,0 +1,6 @@
+export const ADD_TODO = 'ADD_TODO';
+export const DELETE_TODO = 'DELETE_TODO';
+export const DELETE_COMPLETED = 'DELETE_COMPLETED';
+export const EDIT_TODO = 'EDIT_TODO';
+export const COMPLETE_TODO = 'COMPLETE_TODO';
+export const COMPLETE_ALL = 'COMPLETE_ALL';
