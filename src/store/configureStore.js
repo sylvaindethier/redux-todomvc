4eb5d968cc271474afc466686d7885980391ceae
sylvaindethier <dethiersylvain@gmail.com>
2016-01-07 20:21:04 +0100

    improve configureStore bundling

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,15 +1,19 @@
 import { createStore } from 'redux';
 import rootReducer from '../reducers';
 
-export default function configureStoreDEV(initialState) {
+export function configureStore(initialState) {
+  return createStore(rootReducer, initialState);
+}
+
+export function configureStoreHMR(initialState) {
   const store = createStore(rootReducer, initialState);
-  if (module.hot) {
-    // Enable Webpack hot module replacement for reducers
-    module.hot.accept('../reducers', () => {
-      const nextReducer = require('../reducers');
-      store.replaceReducer(nextReducer);
-    });
-  }
+  // Enable Webpack hot module replacement for reducers
+  module.hot.accept('../reducers', () => {
+    const nextReducer = require('../reducers');
+    store.replaceReducer(nextReducer);
+  });
 
   return store;
 }
+
+export default module.hot ? configureStoreHMR : configureStore;
