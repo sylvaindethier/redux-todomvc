c6f55022605c9cb07dac552f2e5ac2472be9f0ff
sylvaindethier <dethiersylvain@gmail.com>
2016-01-09 22:41:10 +0100

    add FILTER actions constant

--- src/constants/actions.js
+++ src/constants/actions.js
@@ -1,6 +1,9 @@
-export const ADD_TODO = 'ADD_TODO';
+// todos actions
+export const CREATE_TODO = 'CREATE_TODO';
+export const UPDATE_TODO = 'UPDATE_TODO';
 export const DELETE_TODO = 'DELETE_TODO';
 export const DELETE_COMPLETED = 'DELETE_COMPLETED';
-export const EDIT_TODO = 'EDIT_TODO';
 export const COMPLETE_TODO = 'COMPLETE_TODO';
 export const COMPLETE_ALL = 'COMPLETE_ALL';
+// filters actions
+export const FILTER = 'FILTER';
