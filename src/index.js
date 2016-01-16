03e9f3802be05381179456f3d229518b0acb4ff6
sylvaindethier <dethiersylvain@gmail.com>
2016-01-16 02:05:22 +0100

    restore initial demo state

--- src/index.js
+++ src/index.js
@@ -7,7 +7,28 @@ import configureStore from './store/configureStore';
 import { store as db } from './utils';
 
 
-const initialState = db() || {};
+const initialDemo = {
+  todos: [
+    {
+      id: 'cf51481f-b40b-4fb4-97d8-741aa7f508fe',
+      done: true,
+      text: 'Use Redux',
+    },
+    {
+      id: '7487576b-5869-435a-9192-939ce028fed3',
+      done: true,
+      text: 'Learn to connect it to React',
+    },
+    {
+      id: '324fd24e-8606-4fa1-8d22-77c6cb956b61',
+      done: false,
+      text: 'Server Side Rendering',
+    },
+  ],
+  filter: 'NONE',
+  editing: { id: '', text: '' },
+};
+const initialState = db() || initialDemo;
 const store = configureStore(initialState);
 render(
   <Root store={store} />,
